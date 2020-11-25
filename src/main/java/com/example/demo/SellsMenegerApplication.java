a2927f3df3bd1c82c80ff127fb8c1c04fcf07306
borislavvv00 <borislav_kalinski@abv.bg>
2020-11-25 15:24:27 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/SellsMenegerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellsMenegerApplication 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SellsMenegerApplication.class, args);
+	}
+}
