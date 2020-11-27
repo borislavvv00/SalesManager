63f83fed994ed9ffcffbf56eb78f056a4b141cf1
borislavvv00 <borislav_kalinski@abv.bg>
2020-11-27 15:45:03 +0200

    Rename main file.

--- src/main/java/com/example/demo/SalesManagerApplication.java
+++ src/main/java/com/example/demo/SalesManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SellsMenegerApplication 
+public class SalesManagerApplication 
 {
 	public static void main(String[] args) 
 	{
-		SpringApplication.run(SellsMenegerApplication.class, args);
+		SpringApplication.run(SalesManagerApplication.class, args);
 	}
 }
